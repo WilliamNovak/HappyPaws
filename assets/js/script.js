2ed526a35354c4caa64e9eea720ce7ac8735616c
William Novak <novakwilliamr@gmail.com>
2024-09-09 20:58:03 -0300

    criada simulacao de login para controlar navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,41 @@ function voltarPagina() {
 
 // Executa ao carregar pagina
 $(document).ready(function() {
-    // Carrega o navbar logada de forma dinamica em todas paginas na regiao do id
-    fetch('loggedNavbar.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('loggedNav').innerHTML = data;
-    });
+
+    // Verifica qual a pagina atual
+    page = window.location.pathname.split('/').pop();
+
+    // Verifica se esta na pagina home
+    if (page === 'index.html') {
+        const urlParams = new URLSearchParams(window.location.search);
+        // Verifica se esta logado
+        const logged = urlParams.get('logged');
+        console.log(logged);
+
+        // Se estiver logado, mostra navbar logado
+        if (logged === 'true') {
+            fetch('loggedNavbar.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('homeNav').innerHTML = data;
+            });
+        } else {
+            // Senao mostra a navbar deslogado
+            fetch('homeNavbar.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('homeNav').innerHTML = data;
+            });
+        }
+
+    } else {
+        // Se nao estiver na home, carrega o navbar logada em todas paginas na regiao do id
+        fetch('loggedNavbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('loggedNav').innerHTML = data;
+        });
+    }
 
     // Carrega o footer de forma dinamica em todas paginas na regiao do id
     fetch('footer.html')
