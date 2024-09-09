85c04ee21ab1f65a68be0ef32f4dc24765465ed4
William Novak <novakwilliamr@gmail.com>
2024-09-09 20:25:57 -0300

    adicionado footer de forma dinamica nas paginas

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,13 @@
 function voltarPagina() {
     window.history.back();
 }
+
+// Executa ao carregar pagina
+$(document).ready(function() {
+    // Carrega o footer de forma dinamica em todas paginas na regiao do id
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('pageFooter').innerHTML = data;
+        });
+})
