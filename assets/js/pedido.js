ead4c853799ecddcb1e09b4c723da81e5ceef2a6
William Novak <novakwilliamr@gmail.com>
2024-09-23 22:17:16 -0300

    resumo pedido dinamico

--- assets/js/pedido.js
+++ assets/js/pedido.js
@@ -12,5 +12,44 @@ $(document).ready(function() {
         } else {
             creditCardDiv.style.display = 'none';
         }
-      });
+    });
+
+    // Busca regiao do resumo do pedido
+    const requestItems = document.getElementById('requestItems');
+    requestItems.innerHTML = '';
+    let price = 0;
+
+    if (localStorage.cart){
+        cart = JSON.parse(localStorage.getItem('cart'));
+    }
+    
+    // Adiciona os itens ao resumo do pedido
+    for (item of cart) {
+        let li = document.createElement('li');
+        if (item.id) {
+            // Busca o produto
+            let product = productsData.find(p => p.id === item.id);
+            li.textContent = `x${item.quantidade} ${product.title}`;
+            // acrescenta o valor dos produtos
+            price += parseFloat(product.price.substring(2).replace(',', '.')) * item.quantidade;
+        } else {
+            // Busca o pet
+            let pet = petsData.find(p => p.id === item.petId);
+            console.log(item)
+            li.textContent = `x1 ${item.service} ${pet.type.toLowerCase()} porte ${pet.size.toLowerCase()}`;
+            // acrescenta o valor dos servicos
+            price += parseFloat(item.price);
+        }
+
+        requestItems.appendChild(li);
+
+        // Atualiza o total dos itens
+        const totalItems = document.getElementById('totalItems');
+        totalItems.innerHTML = `R$${price.toFixed(2).replace('.',',')}`;
+        // Busca o valor do frete
+        const frete = parseFloat(document.getElementById('frete').textContent.substring(2).replace(',','.'));
+        // Atualiza o total do pedido
+        const total = document.getElementById('total');
+        total.innerHTML = `R$${(price + frete).toFixed(2).replace('.',',')}`;
+    }
 });
